ef80dbb44a29abfac1fe68175100604ae69f532f
Phil Burnier <phil.burnier@gmail.com>
2020-05-17 17:10:28 -0500

    Update tabbing to match global editorconfig

--- packages/client/src/App.svelte
+++ packages/client/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
 <nav>
@@ -7,7 +7,7 @@
 </nav>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <h1>Hello {name}!</h1>
 </main>
 
 <style>
@@ -21,23 +21,23 @@
     width: auto;
   }
 
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
